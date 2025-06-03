37e8092ac219592db031b887c175a2e1b666b013
vishesh-here <vishesh@abacus.ai>
2025-06-03 16:14:47 +0530

    Adding Career Section

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <HeroSection />
       <ServicesSection />
       <ProjectsSection />
-      {/* Testing components one by one
       <CareerSection />
+      {/* Testing components one by one
       <TestimonialsSection />
       <AboutSection />
       <ContactSection />
