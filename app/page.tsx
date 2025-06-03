d59f6597d050dbd5df79a6d6fd00d19b6542872b
vishesh-here <vishesh@abacus.ai>
2025-06-03 16:05:49 +0530

    Test except for Projects Section

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,14 @@ import { ContactSection } from '@/components/ContactSection'
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-bold">Portfolio Loading...</h1>
-      </div>
-      {/* Temporarily comment out all sections to test
       <HeroSection />
       <ServicesSection />
-      <ProjectsSection />
       <CareerSection />
       <TestimonialsSection />
       <AboutSection />
       <ContactSection />
+      {/* Testing components one by one
+      <ProjectsSection />
       */}
     </>
   )
